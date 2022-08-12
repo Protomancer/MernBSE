b0cb7dce20f923641e4fefb8ef127c709c7ca292
Billy Pinkerton <billybattlebones@gmail.com>
2022-08-12 13:04:42 -0700

    Updating MernBSE

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -13,6 +13,9 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
+//imported mutation
+const [userLogin, {error}] = useMutation(USER_LOGIN);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -22,25 +25,21 @@ const LoginForm = () => {
     event.preventDefault();
 
     // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    // const form = event.currentTarget;
+    // if (form.checkValidity() === false) {
+    //   event.preventDefault();
+    //   event.stopPropagation();
+    // }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await userLogin({
+        variables: {...userFormData}
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      Auth.login(data.login.token)
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
+    } catch(err) {
       console.error(err);
-      setShowAlert(true);
     }
 
     setUserFormData({
@@ -88,6 +87,7 @@ const LoginForm = () => {
           Submit
         </Button>
       </Form>
+      {error && <div>Failed To Login</div>}
     </>
   );
 };
