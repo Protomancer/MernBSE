0fc1f09b1244145f67d314173ba01ef5739424a7
Billy Pinkerton <billybattlebones@gmail.com>
2022-08-12 14:22:11 -0700

    Updating MernBSE

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { USER_ADD } from '../utils/mutations';
 
 const SignupForm = () => {
   // set initial form state
@@ -12,6 +14,8 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
+const [userAdd] = useMutation(USER_ADD);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -20,23 +24,15 @@ const SignupForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
     try {
-      const response = await createUser(userFormData);
+      const { data } = await userAdd({
+        variables: {...userFormData}
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      Auth.login(data.userAdd.token)
+      
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+
     } catch (err) {
       console.error(err);
       setShowAlert(true);
