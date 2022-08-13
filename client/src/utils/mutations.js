e774b3f5ef7255ce077938f4282ce4d2dbaa769c
Billy Pinkerton <billybattlebones@gmail.com>
2022-08-12 22:55:31 -0700

    Updating MernBSE

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,74 @@
+import gql from 'graphql-tag'
+
+export const USER_LOGIN = gql`
+mutation loginUser($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+    }
+  }
+}
+`;
+
+export const USER_ADD = gql`
+mutation addUser($username: String!, $password: String!, $email: String!) {
+  addUser(username: $username, password: $password, email: $email) {
+    
+    user {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        bookId
+        image
+        link
+        title
+        description
+      }
+    }
+    token
+  }
+}
+`;
+export const BOOK_SAVE = gql`
+    mutation saveBook($input: savedBook!) {
+    saveBook (input: $input)
+        {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
+export const BOOK_REMOVAL = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId:$bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+}
+`;
